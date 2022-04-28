420bbb0acb518802fcd66c28a935f31ce26cdeac
andriano <andrianow817@gmail.com>
2022-04-28 16:23:16 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import icon_dice from "./images/icon-dice.svg";
+import pattern_desk from "./images/pattern-divider-desktop.svg";
+import pattern_mobile from "./images/pattern-divider-mobile.svg";
 function App() {
+  const [quote, setQuote] = useState({});
+  const [active, setActive] = useState(1);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const resp = await fetch(`https://api.adviceslip.com/advice/${active}`);
+      const data = await resp.json();
+      setQuote(data.slip);
+    };
+    fetchAPI();
+    return function () {
+      setQuote({});
+    };
+  }, [active]);
+  const reset = () => {
+    setActive(active + 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="content">
+          <p className="title">
+            <span className="advice-text">ADVICE</span> #{quote.id}
+          </p>
+          <h3 className="quote-text">"{quote.advice}"</h3>
+
+          <img src={pattern_desk} alt="pattern-desk" className="pattern_desk" />
+          <img
+            src={pattern_mobile}
+            alt="pattern-mobile"
+            className="pattern_mobile"
+          />
+          <div className="footer">
+            <button className="btn-api" onClick={reset}>
+              <img src={icon_dice} alt="icon_dice" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="attribution">
+        Challenge by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.frontendmentor.io?ref=challenge"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="https://github.com/Andrian-W">Andrian-W</a>
+      </div>
     </div>
   );
 }
